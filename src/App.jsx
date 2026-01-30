d8ad92d77917b241c5fa88c2e42173d504c6a542
carlosmarte23 <carlosmarte23@gmail.com>
2026-01-30 13:06:07 -0400

    chore: add Home page placeholder

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import AppLayout from "./components/AppLayout";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <AppLayout>
-      <h1>StageLink</h1>
-      <p>MVP in progress</p>
+      <Home />
     </AppLayout>
   );
 }
