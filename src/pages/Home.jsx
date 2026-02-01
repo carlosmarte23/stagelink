445f2311215a6c20195cb945a94cc79c7442a275
carlosmarte23 <carlosmarte23@gmail.com>
2026-02-01 13:40:01 -0400

    feat(home): add mock events dataset

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,16 @@
+import events from "../data/events.json";
+
 export default function Home() {
   return (
     <>
       <h1>StageLink</h1>
-      <p>MVP in progress</p>
+      <div>
+        {events.map((event) => (
+          <div key={event.id}>
+            <h3>{event.title}</h3>
+          </div>
+        ))}
+      </div>
     </>
   );
 }
