68b66dcb046ef0b273ac8f509df92cdb82107d39
carlosmarte23 <carlosmarte23@gmail.com>
2026-04-05 16:44:25 -0400

    fix(config): correct typo on environment key

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   test: {
-    enviromenent: "jsdom",
+    environment: "jsdom",
     setupFiles: "./src/test/setup.js",
     css: true,
   },
