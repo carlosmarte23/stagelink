e7cff1e942bc4fe36cdd1c699823310c0cb9bccf
carlosmarte23 <carlosmarte23@gmail.com>
2026-04-23 19:41:48 -0400

    test(vitest): stabilize checkout tests on windows

--- src/test/setup.js
+++ src/test/setup.js
@@ -1,7 +1,10 @@
 import "@testing-library/jest-dom/vitest";
 import { cleanup } from "@testing-library/react";
-import { afterEach } from "vitest";
+import { afterEach, vi } from "vitest";
+
+window.scroll = vi.fn();
 
 afterEach(() => {
   cleanup();
+  window.scroll.mockClear();
 });
